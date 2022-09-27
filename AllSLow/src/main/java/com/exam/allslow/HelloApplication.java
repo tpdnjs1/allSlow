b3c0d18361470aa2590d310be1f4e851eed148e3
tpdnjs1 <errow0607@gmail.com>
2022-09-27 10:37:43 +0900

    main

--- AllSLow/src/main/java/com/exam/allslow/HelloApplication.java
+++ AllSLow/src/main/java/com/exam/allslow/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 600);
-        stage.setTitle("느루");
+        stage.setTitle("집가고싶다");
         stage.setScene(scene);
         stage.show();
     }
