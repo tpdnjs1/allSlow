3c776210f6f1fa4f5e0eaf81be16134ac4c1cbaa
tpdnjs1 <errow0607@gmail.com>
2022-09-28 15:35:43 +0900

    fuckingError

--- AllSLow/src/main/java/com/exam/allslow/HelloApplication.java
+++ AllSLow/src/main/java/com/exam/allslow/HelloApplication.java
@@ -3,12 +3,16 @@ package com.exam.allslow;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(getClass().getResourceAsStream("/resources/EF_Diary.ttf"), 16);
+        Font.loadFont(getClass().getResourceAsStream("/resources/KyoboHandwriting2020pdy.ttf"), 16);
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 600);
         stage.setTitle("느루");
@@ -17,6 +21,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- AllSLow/src/main/java/com/exam/allslow/MainController.java
+++ AllSLow/src/main/java/com/exam/allslow/MainController.java
@@ -1,5 +1,57 @@
 package com.exam.allslow;
 
-public class MainController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private void calendarPage(MouseEvent event){
+        loadPage("calender");
+    }
+
+    @FXML
+    private void communityPage(MouseEvent event){
+        loadPage("community");
+    }
+
+    @FXML
+    private void exercisePage(MouseEvent event){
+        loadPage("exercise");
+    }
+
+    @FXML
+    private void foodPage(MouseEvent event){
+        loadPage("food");
+    }
+
+    @FXML
+    private void settingPage(MouseEvent event){
+        loadPage("setting");
+    }
+
+    @FXML
+    private BorderPane pane;
+    public void loadPage(String page) {
+        try {
+            Parent nextScene = FXMLLoader.load(getClass().getResource(page+".fxml"));
+            pane.setCenter(nextScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
