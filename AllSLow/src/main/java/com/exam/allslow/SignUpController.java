1e32ea9bfe7a15e3bdb5903d6a05c00fb7f935f4
tpdnjs1 <errow0607@gmail.com>
2022-10-19 15:38:31 +0900

    signUp

--- AllSLow/src/main/java/com/exam/allslow/SignUpController.java
+++ AllSLow/src/main/java/com/exam/allslow/SignUpController.java
@@ -5,10 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -19,7 +16,9 @@ public class SignUpController implements Initializable {
     @FXML
     private TextField id;
     @FXML
-    private TextField pw;
+    private PasswordField pw;
+    @FXML
+    private PasswordField checkPw;
     @FXML
     private TextField email;
     @FXML
@@ -47,13 +46,15 @@ public class SignUpController implements Initializable {
     @FXML
     private Label warnSize;
 
+    private boolean warn;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         sex.getItems().addAll("남성", "여성");
 
     }
 
-    public void backLgn(){
+    public void backLgn() {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("signIn.fxml"));
             Scene scene = new Scene(root);
@@ -64,14 +65,72 @@ public class SignUpController implements Initializable {
         }
     }
 
-    public void signUp(){
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) signUpBtn.getScene().getWindow();
-            stage.setScene(scene);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void signUp() {
+        canSignUp();
+        if (!warn) {
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) signUpBtn.getScene().getWindow();
+                stage.setScene(scene);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
+
+    private void canSignUp() {
+        int pass = 0;
+
+        if (id.getText().isBlank()) {
+            warnId.setText("*아이디가 입력되지 않았습니다.");
+        /*} else if () {
+            warnId.setText("*동일한 아이디가 존재합니다.");
+        */
+        } else {
+            warnId.setText("");
+            pass++;
+        }
+
+        if (pw.getText().isBlank()) {
+            warnPw.setText("*비밀번호가 입력되지 않았습니다.");
+        } else if (!pw.getText().equals(checkPw.getText())) {
+            warnPw.setText("*입력된 비밀번호와 일치하지않습니다.");
+        } else {
+            warnPw.setText("");
+            pass++;
+        }
+
+
+        if (sex.getValue() == null) {
+            warnSex.setText("*성별이 선택되지 않았습니다.");
+        } else {
+            warnSex.setText("");
+            pass++;
+        }
+
+
+        if (age.getText().isBlank()) {
+            warnAge.setText("*나이가 입력되지 않았습니다.");
+        } else {
+            warnAge.setText("*숫자 외 다른 문자가 입력되었습니다.");
+        }
+        warnAge.setText("");
+        pass++;
+
+        if (tall.getText().isBlank() || weight.getText().isBlank()) {
+            warnSize.setText("*키 또는 몸무게가 입력되지 않았습니다.");
+        } else {
+            warnSize.setText("");
+            pass++;
+        }
+
+        if (pass == 5) {
+            warn = false;
+        } else {
+            warn = true;
+        }
+    }
+
+
 }
