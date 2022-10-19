8a0c5545f162880653d94d96fd32c8054fc37e7d
tpdnjs1 <errow0607@gmail.com>
2022-10-19 11:16:07 +0900

    signUp

--- AllSLow/src/main/java/com/exam/allslow/SignUpController.java
+++ AllSLow/src/main/java/com/exam/allslow/SignUpController.java
@@ -1,12 +1,33 @@
 package com.exam.allslow;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class SignUpController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class SignUpController implements Initializable {
 
+    @FXML
+    private TextField id;
+    @FXML
+    private TextField pw;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField age;
+    @FXML
+    private TextField tall;
+    @FXML
+    private TextField weight;
+    @FXML
+    private ChoiceBox<String> sex = new ChoiceBox<>();
 
-
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        sex.getItems().addAll("남성","여성");
+    }
 }
